892c82d3ed4bfdd636f29f809c06d89b0771bd75
Pat White <pat.white@synata.com>
2015-03-16 12:29:05 -0700

    Working project

--- src/main/scala/com/synata/Analyzer.scala
+++ src/main/scala/com/synata/Analyzer.scala
@@ -2,7 +2,8 @@ package com.synata
 
 import com.mongodb.BasicDBList
 import org.apache.hadoop.conf.Configuration
-import org.apache.spark.graphx.{EdgeDirection, Graph, Edge}
+import org.apache.spark.graphx._
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 import org.bson.BSONObject
 import org.bson.types.BasicBSONList
@@ -16,15 +17,193 @@ import java.io._
 
 case class Email(id: String, subject: String, from: List[String], to: List[String], cc: List[String], bcc: List[String])
 
-
+case class CountTracker(inFromSource: Int, outToSource: Int) {
+  def getRatio = if(outToSource == 0) Double.NegativeInfinity else inFromSource.toDouble / outToSource.toDouble
+  def total = inFromSource + outToSource
+  def incIn: CountTracker = copy(inFromSource + 1, outToSource)
+  def incOut: CountTracker = copy(inFromSource, outToSource + 1)
+  def merge(toMerge: CountTracker): CountTracker = {
+    val out = CountTracker(toMerge.inFromSource + inFromSource, toMerge.outToSource + outToSource)
+    out
+  }
+}
 
 class Analyzer {
+  private def getContext = {
+    val driverPort = 7777
+    val driverHost = "localhost"
+    val master = "local"
+    val app = "Spark Graph Analysis"
+
+    val conf = new SparkConf() // skip loading external settings
+      .set("spark.logConf", "true")
+      .set("spark.driver.host", s"$driverHost")
+      .set("spark.driver.port", s"$driverPort")
+      .set("spark.cores.max", "2")
+      .set("spark.executor.memory", "4g")
+      .set("spark.eventLog.enabled", "false")
+    val context = new SparkContext(master, app, conf)
+
+    // Context setup code will go here
+    val jarFile = "target/scala-2.10/SparkGraphAnalysis-assembly-1.0.jar"
+    val mongoDriverFile = "lib/mongo-java-driver-2.12.4.jar"
+    val mongoHadoopFile = "lib/mongo-hadoop-core-1.3.0.jar"
+
+    context.addJar(jarFile)
+    context.addJar(mongoDriverFile)
+    context.addJar(mongoHadoopFile)
+
+    context
+  }
+
   def exec() = {
+    val context = getContext
+    try {
+      //Add to the exec function
+      val mongoUri = s"mongodb://localhost"
+      val hadoopConfig = new Configuration()
+
+      hadoopConfig.set("mongo.input.uri", s"$mongoUri/graph.data")
+      hadoopConfig.set("mongo.output.uri", s"$mongoUri/analysis.output")
+
+
+      val rdd = context.newAPIHadoopRDD(hadoopConfig, classOf[com.mongodb.hadoop.MongoInputFormat], classOf[Object], classOf[BSONObject])
+
+      val mapped = rdd.map(m => Analyzer.parseObj(m._2))
+      val emailListRDD = mapped.flatMap(m => m.from ::: m.to ::: m.cc ::: m.bcc)
+        .distinct()
+        .zipWithIndex()
+        .map(m => (m._2, m._1))
+
+      val emailLookup = emailListRDD.collect().toMap
+      val idLookup = emailLookup.map(m => (m._2, m._1)).toMap
+
+      val edges = mapped.flatMap(m => Analyzer.parseEdge(m, idLookup))
+
+      val g = Graph(emailListRDD, edges)
+
+      Analyzer.outputGraph("MainGraph.gml", g)
+
+      val me = calculatePageRankAndGetSelf(g, emailListRDD)
+
+      calculateStronglyConnected(me, g, emailLookup)
+
+      calculateCRS(me, g, emailListRDD)
+
+    } finally {
+      context.stop()
+    }
+
     println("All Done")
+  }
+
+  def calculatePageRankAndGetSelf(g: Graph[String, String], emailListRDD: RDD[(Long, String)]): (Long, String) = {
+    val pageRanked = g.pageRank(0.01).vertices
+
+    val ranked = pageRanked.join(emailListRDD)
+      .sortBy(m => m._2._1, ascending = false)
+
+    val prOutput = ranked.take(100) //Only grab the top 100
+    prOutput.foreach { m =>
+      println(s"${m._2._2}: ${m._2._1}")
+    }
+
+    val me = (prOutput.head._1, prOutput.head._2._2)
+
+    println(s"I am: $me")
+
+    me
+  }
+
+
+  //Add to the Analyzer object
+  def calculateStronglyConnected(me: (Long, String), g: Graph[String, String], emailLookup: Map[Long, String]) = {
+    val stronglyConnected = g.filter(m => m, vpred = (id, m: String) => id != me._1).stronglyConnectedComponents(10)
+
+    val strongGraph = stronglyConnected.collectEdges(EdgeDirection.Out)
+
+    strongGraph.collect().foreach { e =>
+      val email = emailLookup(e._1)
+      println(s"$email is strongly connected to:")
+      val connected = e._2.map { inner =>
+        if(inner.srcId == e._1) {
+          emailLookup(inner.dstId)
+        } else {
+          emailLookup(inner.srcId)
+        }
+      }
+      connected.distinct.foreach(c => println("\t" + c))
+    }
+  }
+
+  def calculateCRS(me: (Long, String), g: Graph[String, String], emailListRDD: RDD[(Long, String)]) = {
+    //Pregel
+    def processMessages = (src: VertexId, currentCount: CountTracker, message: CountTracker) => {
+      currentCount.merge(message)
+    }
+
+    def sendMessages = (triplet: EdgeTriplet[CountTracker, String]) => {
+      if(triplet.srcId == me._1) {
+        //Send to dest
+        Iterator((triplet.dstId, CountTracker(1, 0)))
+      } else if(triplet.dstId == me._1) {
+        //Send to src
+        Iterator((triplet.srcId, CountTracker(0, 1)))
+      } else {
+        Iterator.empty
+      }
+    }
+
+    def mergeMessages = (a: CountTracker, b: CountTracker) => {
+      a.merge(b)
+    }
+
+    val initialGraph = g.mapVertices((id, _) => CountTracker(0,0))
+    val crs = initialGraph.pregel(CountTracker(0,0), activeDirection = EdgeDirection.Both, maxIterations = 1)(
+      processMessages, // Vertex Program
+      sendMessages,
+      mergeMessages
+    )
+
+    val orderedCRS = crs.vertices.join(emailListRDD)
+      .map(m => {
+      val ratio = m._2._1.getRatio
+      val total = m._2._1.total
+      val mult = if(ratio.isInfinite) Double.NegativeInfinity else ratio * total
+      (m._2._2, ratio, math.abs(1 - ratio), mult)
+    }).filter(m => m._2 != Double.NegativeInfinity && m._2 != 0D)
+
+
+    println(orderedCRS.sortBy(m => m._3).collect.mkString("\n"))
+
+    println("------------")
+
+    println(orderedCRS.sortBy(m => m._4, ascending = true).collect.mkString("\n"))
   }
 }
 
 object Analyzer {
+  def parseObj(obj: BSONObject): Email = {
+    val from = if(obj.containsField("From")) obj.get("From").asInstanceOf[BasicDBList].map(_.toString).toList else List()
+    val to = if(obj.containsField("To")) obj.get("To").asInstanceOf[BasicDBList].map(_.toString).toList else List()
+    val cc = if(obj.containsField("CC")) obj.get("CC").asInstanceOf[BasicDBList].map(_.toString).toList else List()
+    val bcc = if(obj.containsField("BCC")) obj.get("BCC").asInstanceOf[BasicDBList].map(_.toString).toList else List()
+
+    val subject = obj.get("Subject").toString
+    val id = obj.get("_id").toString
+    Email(id, subject, from, to, cc, bcc)
+  }
+
+  def parseEdge(obj: Email, lookup: Map[String, Long]): List[Edge[String]] = {
+    obj.from.flatMap { f =>
+      val fId = lookup(f)
+      val to = obj.to.map(t => Edge(fId, lookup(t), "SentEmailTo"))
+      val cc = obj.cc.map(t => Edge(fId, lookup(t), "CCed"))
+      val bcc = obj.bcc.map(t => Edge(fId, lookup(t), "BCCed"))
+      to ::: cc ::: bcc
+    }
+  }
+
   def outputGraph(filename: String, graph: Graph[_, String]) = {
     val nodes = graph.vertices.collect()
     val edges = graph.edges.collect()
